62de82ca558480e7bd4bb929ac9af1de3d8e4cf0
hDoubleE <Trevor.Heehs@stmartin.edu>
2019-01-24 09:44:13 -0800

    modified files

---
+++ C-Sharp/mergeSort.cs
@@ -0,0 +1,19 @@
+namespace Sorting
+{
+    static void Main()
+    {
+        int[] sampleArray = {5, 13, 8, 27, 43, 12, 23};
+    }
+    public class Sort
+    {
+        public static void MergeSortHelper(int[] arr)
+        {
+            MergeSort(arr, 0, arr.Length - 1);
+        }
+
+        public static void MergeSort(int[] arr, int start, int end)
+        {
+
+        }
+    }
+}
